481f92aca309883ad8ea9dc8fa39370dffba1101
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2024-05-31 11:58:08 +0200

    Added the blockFor function

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
+const urlParams = new URLSearchParams(window.location.search);
+const blockForParam = Number(urlParams.get("blockFor")) || 0;
+
+const blockFor = (ms) => {
+  const target = performance.now() + ms;
+  while (performance.now() < target);
+};
+
 document.addEventListener("DOMContentLoaded", function () {
   const headers = document.querySelectorAll(".accordion-header");
 
   headers.forEach((header) => {
     header.addEventListener("click", function () {
+      blockFor(blockForParam);
+      
       const content = header.nextElementSibling;
-
       headers.forEach((h) => {
         if (h !== header) {
           h.nextElementSibling.style.maxHeight = null;
